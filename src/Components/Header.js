4ea0ac8b94f43987b45dacd25e1486de7d192112
Nick <nickmagi24@gmail.com>
2021-11-01 18:01:36 -0400

    Header

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+
+        return(
+            <div className="header">
+                <h2 className="title"> CV BUILDER</h2>
+            </div>
+        )
+    };
+}
+
+export default Header;
